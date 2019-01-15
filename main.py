417bb16f64c316eeeccfb0ac46ee8e8560fa1f57
Jakub <ksobczak16@gmail.com>
2019-01-15 19:10:55 +0000

    lcd.py as main.py now, all working except points refresh

--- main.py
+++ main.py
@@ -1,25 +1,75 @@
-#/usr/bin/python
+#!/usr/bin/python3
 import time
-import RPi.GPIO as GPIO
+import gpiozero
 import SimpleMFRC522
 import mysql.connector as connector
-import Adafruit_CharLCD as LCD
-from mysql.connector import errorcode
-# Raspberry Pi pin configuration:
-lcd_rs        = 26 
-lcd_en        = 19
-lcd_d4        = 13
-lcd_d5        = 6
-lcd_d6        = 21
-lcd_d7        = 20
-lcd_backlight = 2
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd as character_lcd
 
+from mysql.connector import errorcode
+import threading
+# Raspberry Pi pin configuration:
+
+btn1 = 16
+btn2 = 12
+btn3 = 1
+
+lcd_rs = digitalio.DigitalInOut(board.D26)
+lcd_en = digitalio.DigitalInOut(board.D19)
+lcd_d7 = digitalio.DigitalInOut(board.D20)
+lcd_d6 = digitalio.DigitalInOut(board.D21)
+lcd_d5 = digitalio.DigitalInOut(board.D6)
+lcd_d4 = digitalio.DigitalInOut(board.D13)
+lcd_backlight = digitalio.DigitalInOut(board.D2)
 lcd_columns = 16
 lcd_rows    = 2
+lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+
+id = (None, None)
+points_new = 0
+counter = False
+reader = SimpleMFRC522.SimpleMFRC522()
+
+
+def btn_plus():
+    global points_new
+    global counter
+    points_new +=1
+    counter = True
+
+def btn_minus():
+    global points_new
+    global counter
+    points_new-=10
+    counter = True
+
+def btn_reset():
+    global points_new
+    global counter
+    points_new = 0
+    counter = False
+
+
+button1 = gpiozero.Button(btn1)
+button2 = gpiozero.Button(btn2)
+button3 = gpiozero.Button(btn3)
+
+button1.when_released = btn_plus
+button2.when_released = btn_minus
+button3.when_released = btn_reset
+
+
+print("Connecting to Database")
+lcd.clear()
+lcd.message = "Connecting to \nDatabase" 
+
+
 try:
     con = connector.connect(user='alicjamus_baza1', password='cQ2Qvg8U23', host='95.216.64.27', database='alicjamus_baza1')
     cursor = con.cursor()
-        
+    time.sleep(2.0)        
 except connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print('Access denied')
@@ -29,38 +79,62 @@ except connector.Error as err:
         print(err)
 finally:
     print('connected')
-    
-reader = SimpleMFRC522.SimpleMFRC522()
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                           lcd_columns, lcd_rows, lcd_backlight)
+
+class CardThread(threading.Thread):
+    def __init__(self):
+        super(CardThread, self).__init__()
+    def run(self):
+        while True:
+            global id
+            id = reader.read_id()
+            print(id)
+
+
+thread1 = CardThread()
+thread1.setDaemon(True)
+thread1.start()
 
 try:
+    lcd.clear()
     while True:
-            lcd.message("Przyloz Karte")
-            id, data = reader.read()
-            cursor.execute("select points from Users where cardId='{}'".format(id))
-            points = cursor.fetchall()
-            if(not points):
-                cursor.execute("insert into Users (cardId) values ({})".format(id))
-                con.commit()
-                lcd.clear()
-                lcd.message("Dodano karte\nklienta")
-                print(cursor.rowcount,"Not in database, adding")
-                time.sleep(5.0)
-            elif(points):
-                points = points[0][0]
-                print(points)
-                lcd.clear()
-                lcd.message('Punkty:{}'.format(str(points)))
-                time.sleep(3)
-                lcd.clear()
+            lcd.message = "Przyloz Karte"
+            print("imalive")
+            while counter == True:
+                lcd.message = "Points +{}".format(points_new)
+                if id != (None, None):
+                    cursor.execute("select points from Users where cardId='{}'".format(id))
+                    points = cursor.fetchall()
+                    points_new = points[0][0] + points_new
+                    cursor.execute("update Users set points = {} where cardId = {}".format(points_new, id))
+                    con.commit()
+                    counter = False
+                    points_new = 0
+                    break
+            if id != (None, None):
+                cursor.execute("select points from Users where cardId='{}'".format(id))
+                points = cursor.fetchall()
+                id = None 
+                if(not points):
+                    cursor.execute("insert into Users (cardId) values ({})".format(id))
+                    con.commit()
+                    lcd.clear()
+                    lcd.message= "Dodano karte\nklienta"
+                    print(cursor.rowcount,"Not in database, adding")
+                    time.sleep(5.0)
+                elif(points):
+                    points = points[0][0]
+                    print(points)
+                    lcd.clear()
+                    lcd.message = 'Punkty:{}'.format(str(points))
+                    time.sleep(3)
+                    lcd.clear()
         
 finally:
+        thread1.join(0)
         lcd.clear()
-        lcd.message('Bay')
+        lcd.message = 'Bay'
         time.sleep(2.0)
         lcd.clear()
-        GPIO.cleanup()
         con.close()
         
 
